3a61c6b90c8e5bb9a8a6203ee5ab82f79418a94a
Tina <tinabeyki77@gmail.com>
2022-02-27 16:20:52 +0330

    add phpCheat

--- php/index.php
+++ php/index.php
@@ -1,3 +1,20 @@
 <?php
-echo "Hello Tina";
+// Single line comment
+# Single line comment
+/* Multi line comments */
+
+
+$wheels = ['bicycle' => 2, 'car' => 4];
+
+// Foreach loops can iterate over arrays
+foreach ($wheels as $wheel_count) {
+    echo $wheel_count;
+} // Prints "24"
+
+echo "\n";
+
+// You can iterate over the keys as well as the values
+foreach ($wheels as $vehicle => $wheel_count) {
+    echo "A $vehicle has $wheel_count wheels";
+}
 ?>
